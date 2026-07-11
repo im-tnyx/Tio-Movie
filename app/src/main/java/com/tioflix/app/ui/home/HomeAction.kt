223c1f5c9e170612be6591b2c18a008b4fc29666
Santosh Jangid <tnyxapp@gmail.com>
2026-07-12 01:52:27 +0530

    Add content selection action

--- app/src/main/java/com/tioflix/app/ui/home/HomeAction.kt
+++ app/src/main/java/com/tioflix/app/ui/home/HomeAction.kt
@@ -3,4 +3,5 @@ package com.tioflix.app.ui.home
 sealed interface HomeAction {
     data object RetryClicked : HomeAction
     data object LogoutClicked : HomeAction
+    data class ContentClicked(val contentId: String) : HomeAction
 }
