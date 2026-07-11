a48f17939d0025b30e8abd75ccf24cb50b8c2f4b
Santosh Jangid <tnyxapp@gmail.com>
2026-07-11 17:46:11 +0530

    Add home ViewModel

---
+++ app/src/main/java/com/tioflix/app/ui/home/HomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.tioflix.app.ui.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+    fun onAction(action: HomeAction) {
+        when (action) {
+            HomeAction.RetryClicked -> Unit
+        }
+    }
+}
