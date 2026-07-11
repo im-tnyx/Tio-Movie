5f6e5a5414a03a71becac2fbf111b744dba7bdcb
Santosh Jangid <tnyxapp@gmail.com>
2026-07-11 18:18:58 +0530

    Add Google client configuration boundary

--- app/src/main/java/com/tioflix/app/core/config/AppConfig.kt
+++ app/src/main/java/com/tioflix/app/core/config/AppConfig.kt
@@ -5,7 +5,11 @@ import com.tioflix.app.BuildConfig
 object AppConfig {
     val supabaseUrl: String get() = BuildConfig.SUPABASE_URL
     val supabasePublishableKey: String get() = BuildConfig.SUPABASE_PUBLISHABLE_KEY
+    val googleWebClientId: String get() = BuildConfig.GOOGLE_WEB_CLIENT_ID
 
     val isSupabaseConfigured: Boolean
         get() = supabaseUrl.isNotBlank() && supabasePublishableKey.isNotBlank()
+
+    val isGoogleSignInConfigured: Boolean
+        get() = isSupabaseConfigured && googleWebClientId.isNotBlank()
 }
