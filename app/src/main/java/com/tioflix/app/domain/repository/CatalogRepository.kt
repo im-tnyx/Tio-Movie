5c85ca8f0198c628309dbc107f318842bb122ead
Santosh Jangid <tnyxapp@gmail.com>
2026-07-12 10:09:04 +0530

    Add content detail repository query

--- app/src/main/java/com/tioflix/app/domain/repository/CatalogRepository.kt
+++ app/src/main/java/com/tioflix/app/domain/repository/CatalogRepository.kt
@@ -1,9 +1,11 @@
 package com.tioflix.app.domain.repository
 
+import com.tioflix.app.domain.model.ContentItem
 import com.tioflix.app.domain.model.HomeCatalog
 import com.tioflix.app.domain.model.SeriesSeason
 
 interface CatalogRepository {
     suspend fun getHomeCatalog(): Result<HomeCatalog>
+    suspend fun getContent(contentId: String): Result<ContentItem>
     suspend fun getSeriesSeasons(contentId: String): Result<List<SeriesSeason>>
 }
