b3ac32852d0393ce3b4cd2bdc7a162c527ec2518
Santosh Jangid <tnyxapp@gmail.com>
2026-07-16 13:37:19 +0530

    Add search navigation effect

--- app/src/main/java/com/tioflix/app/ui/home/HomeEffect.kt
+++ app/src/main/java/com/tioflix/app/ui/home/HomeEffect.kt
@@ -2,5 +2,6 @@ package com.tioflix.app.ui.home
 
 sealed interface HomeEffect {
     data object NavigateLogin : HomeEffect
+    data object NavigateSearch : HomeEffect
     data class NavigateContentDetail(val contentId: String) : HomeEffect
 }
